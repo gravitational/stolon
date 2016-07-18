c489f1c4423d9130310305783778db519b92e922
Jeff Zellner <jeffz@eml.cc>
2016-07-18 17:25:31 -0600

    quick and dirty s3 backup

--- cmd/stolonctl/postgresql/postgresql.go
+++ cmd/stolonctl/postgresql/postgresql.go
@@ -26,6 +26,12 @@ type S3Settings struct {
 	SecretAccessKey string
 }
 
+type S3Location struct {
+	Host   string
+	Bucket string
+	Path   string
+}
+
 func Backup(connSettings ConnSettings, s3Settings S3Settings, dbName string, backupPath string) error {
 	fileName := fmt.Sprintf(`%v_%v.sql.gz`, dbName, time.Now().Unix())
 	if strings.HasPrefix(backupPath, "s3://") {
@@ -71,26 +77,49 @@ func backupToFile(connSettings ConnSettings, dbName string, fileName string) err
 }
 
 func uploadToS3(s3Settings S3Settings, sourceFilename string, destination string) error {
-	ssl := false
+	ssl := true
 
-	url, err := url.Parse(destination)
+	location, err := parseS3Location(destination)
 	if err != nil {
 		return trace.Wrap(err)
 	}
 
-	client, err := minio.New(url.Host, s3Settings.AccessKeyID, s3Settings.SecretAccessKey, ssl)
+	client, err := minio.New(location.Host, s3Settings.AccessKeyID, s3Settings.SecretAccessKey, ssl)
 	if err != nil {
 		return trace.Wrap(err)
 	}
 
 	_, filename := path.Split(sourceFilename)
 
-	n, err := client.FPutObject(url.Path, filename, sourceFilename, "application/gzip")
+	log.Infof("bucket: %v", location.Bucket)
+
+	n, err := client.FPutObject(location.Bucket, filename, sourceFilename, "application/gzip")
 	if err != nil {
 		return trace.Wrap(err)
 	}
-	log.Printf("Successfully uploaded %s of size %d\n", filename, n)
+	log.Infof("Successfully uploaded %s of size %d\n", filename, n)
 	return nil
+}
+
+func parseS3Location(path string) (*S3Location, error) {
+	s3 := &S3Location{}
+	url, err := url.Parse(path)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	s3.Host = url.Host
+	log.Infof("backup host: %v", s3.Host)
+
+	splitPath := strings.Split(strings.TrimPrefix(url.Path, "/"), "/")
+	s3.Bucket = splitPath[0]
+	log.Infof("backup bucket: %v", s3.Bucket)
+
+	if len(splitPath) > 1 {
+		s3.Path = strings.Join(splitPath[1:], "")
+	}
+	log.Infof("backup path: %v", s3.Path)
+	return s3, nil
 }
 
 func pgDumpCommand(args ...string) *exec.Cmd {
