74f058139c81ffe7cfa76c4879ef7f8bde8b2b86
Anton Galitsyn <anton.galitsyn@gmail.com>
2016-08-30 19:32:10 +0700

    add RPC delete operation

--- cmd/stolonrpc/operations.go
+++ cmd/stolonrpc/operations.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/gravitational/stolon/cmd/stolonctl/database"
 	"github.com/gravitational/trace"
@@ -17,6 +19,17 @@ func (o *Operation) Create(name string, reply *string) error {
 		return trace.Wrap(err)
 	}
 
-	*reply = "Success"
+	*reply = fmt.Sprintf("DB '%s' was successfully created", name)
+	return nil
+}
+
+func (o *Operation) Delete(name string, reply *string) error {
+	log.Infof("Execute: delete %s", name)
+	err := database.Delete(o.ConnSettings, name)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	*reply = fmt.Sprintf("DB '%s' was successfully deleted", name)
 	return nil
 }
