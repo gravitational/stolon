bbc06336b011c9e7aa689eeda7a7d3d4c88840a4
Sergei Antipov <sergei@gravitational.com>
2020-10-09 12:15:09 +0700

    Sleep waiting replication slot to be created on master

--- cmd/keeper/keeper.go
+++ cmd/keeper/keeper.go
@@ -773,6 +773,8 @@ func (p *PostgresKeeper) postgresKeeperSM(pctx context.Context) {
 	if !ok {
 		// No information about our role
 		log.Infof("our keeper requested role is not available")
+		// sleep a few cycles to make sure replication slot is created on master
+		time.Sleep(2 * clusterConfig.SleepInterval)
 		if initialized && !started {
 			if err = pgm.Start(); err != nil {
 				log.Errorf("failed to start postgres: %v", err)
